d1f89d8ab22a930a496110751af70de460711c13
geckoslair <enrico@piramid.studio>
2019-05-06 20:41:58 +0200

    added cypress for e2e play

---
+++ tests/e2e/specs/article.js
@@ -0,0 +1,5 @@
+describe("Article lifecycle", () => {
+  it("User should create an article", () => {});
+  it("User should edit an article", () => {});
+  it("User should delete an article", () => {});
+});
---
+++ tests/e2e/specs/user.js
@@ -0,0 +1,5 @@
+describe("User lifecycle", () => {
+  it("User should signup", () => {});
+  it("User should login", () => {});
+  it("User should logout", () => {});
+});
