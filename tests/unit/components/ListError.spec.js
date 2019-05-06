b45abb1630d000dd9e946a082091fe3ea48b0f93
geckoslair <enrico@piramid.studio>
2019-05-06 20:11:40 +0200

    empty test

--- tests/unit/components/ListError.spec.js
+++ tests/unit/components/ListError.spec.js
@@ -1,47 +1,7 @@
-import { mount } from "@vue/test-utils";
-
-import ListErrors from "../../../src/components/ListErrors.vue";
-
-const createWrapper = ({ errors }) => {
-  return mount(ListErrors, {
-    propsData: {
-      errors
-    }
-  });
-};
-
 describe("ListErrors", () => {
-  let errors;
+  it("should display the correct error messages based on object from props", () => {});
 
-  beforeEach(() => {
-    errors = {
-      title: ["Title Error"],
-      body: ["can't be blank"],
-      description: ["can't be blank"]
-    };
-  });
+  it("should have props with errors as type object", () => {});
 
-  it("should display the correct error messages based on object from props", () => {
-    const wrapper = createWrapper({ errors });
-
-    const errorMessages = wrapper.findAll("li");
-    expect(errorMessages.length).toEqual(3);
-    expect(errorMessages.at(0).text()).toContain(errors.title);
-    expect(errorMessages.at(1).text()).toContain(errors.body);
-    expect(errorMessages.at(2).text()).toContain(errors.description);
-  });
-
-  it("should have props with errors as type object", () => {
-    const wrapper = createWrapper({ errors });
-    expect(typeof wrapper.props().errors).toBe("object");
-  });
-
-  it("should have no errors if no errors are passed into the props", () => {
-    errors = {};
-
-    const wrapper = createWrapper({ errors });
-
-    const errorMessages = wrapper.findAll("li");
-    expect(errorMessages.length).toEqual(0);
-  });
+  it("should have no errors if no errors are passed into the props", () => {});
 });
